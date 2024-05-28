60984f6e549ba64679c709c79cf3598998042586
imjesusc <iamjesus.ds@gmail.com>
2024-05-28 00:09:52 +0000

    Init project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,82 @@
+import type { Config } from 'tailwindcss'
+
+const config = {
+  darkMode: ['class'],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        '2xl': '1200px'
+      }
+    },
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      keyframes: {
+        'fade-in': {
+          from: {
+            opacity: '0'
+          },
+          to: { opacity: '1' }
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-out forwards'
+      }
+    },
+    screens: {
+      phone: '414px',
+      mobile: '640px',
+      tablet: '768px',
+      tabletlg: '960px',
+      tabletxl: '1024px',
+      laptop: '1200px',
+      laptoplg: '1400px',
+      desktop: '1700px'
+    }
+  },
+  plugins: [require('tailwindcss-animate')]
+} satisfies Config
+
+export default config
