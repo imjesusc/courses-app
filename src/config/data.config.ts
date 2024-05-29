e05fd20a93f6a55df8e8a7d1a703ad23d0b0d260
imjesusc <iamjesus.ds@gmail.com>
2024-05-28 20:08:14 -0400

    feat: Add data.config file

---
+++ src/config/data.config.ts
@@ -0,0 +1,31 @@
+export const courseCategories: { id: string; label: string; slug: string }[] = [
+  { id: '1', label: 'Desarrollo Web', slug: 'desarrollo-web' },
+  { id: '2', label: 'Ciencia de Datos', slug: 'ciencia-de-datos' },
+  { id: '3', label: 'Diseño Gráfico', slug: 'diseno-grafico' },
+  { id: '4', label: 'Negocios', slug: 'negocios' },
+  { id: '5', label: 'Idiomas', slug: 'idiomas' },
+  { id: '6', label: 'Desarrollo Personal', slug: 'desarrollo-personal' },
+  { id: '7', label: 'Tecnología', slug: 'tecnologia' },
+  { id: '8', label: 'Arte y Música', slug: 'arte-y-musica' },
+  { id: '9', label: 'Salud y Bienestar', slug: 'salud-y-bienestar' },
+  { id: '10', label: 'Educación', slug: 'educacion' }
+]
+
+export const navLinks: { label: string; href: string }[] = [
+  {
+    label: 'Inicio',
+    href: '/'
+  },
+  {
+    label: 'Populares',
+    href: '/populares'
+  },
+  {
+    label: 'Explorar',
+    href: '/explorar'
+  },
+  {
+    label: 'Mi lista',
+    href: '/mi-lista'
+  }
+]
