1cab50a60452f3289dedea21fc1472f1924b04ac
imjesusc <iamjesus.ds@gmail.com>
2024-05-28 23:23:38 -0400

    feat: Add userFilterCourses hook

---
+++ src/modules/explorar/hooks/useFilterCourses.tsx
@@ -0,0 +1,71 @@
+import { hasItems } from '@/utilities'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+
+export function useFilterCourses() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+
+  // Categories state
+  const [categories, setCategories] = useState<string[]>(() => {
+    const params = new URLSearchParams(searchParams)
+    const categories = params.get('categories')
+    return categories ? categories.split(',') : []
+  })
+
+  // Update categories state and categories params
+  const handleUpdateCategory = useDebouncedCallback((category: string) => {
+    const params = new URLSearchParams(searchParams)
+    const prevTerm = params.get('q')
+    params.delete('q')
+    params.delete('categories')
+
+    prevTerm && params.set('q', prevTerm)
+
+    // Check if category already exists
+    if (!categories.includes(category)) {
+      const newCategories = [...categories, category]
+      params.set('categories', newCategories.join(','))
+      setCategories(newCategories)
+      return replace(`${pathname}?${params.toString()}`)
+    }
+
+    // If category already exists, remove it
+    const updatedCategories = categories.filter((c) => c !== category)
+    hasItems(updatedCategories) && params.set('categories', updatedCategories.join(','))
+    setCategories(updatedCategories || [])
+
+    return replace(`${pathname}?${params.toString()}`)
+  }, 250)
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams)
+    const prevCategories = params.get('categories')
+
+    params.delete('q')
+    params.delete('categories')
+
+    // Check if category already exists and if category already exists set the previous categories
+    if (term) params.set('q', term)
+    prevCategories && params.set('categories', prevCategories)
+
+    return replace(`${pathname}?${params.toString()}`)
+  }, 250)
+
+  const handleReset = () => {
+    const params = new URLSearchParams(searchParams)
+    params.delete('categories')
+    replace(`${pathname}?${params.toString()}`)
+    setCategories([])
+  }
+
+  return {
+    categories,
+    handleReset,
+    handleUpdateCategory,
+    searchParams,
+    handleSearch
+  }
+}
