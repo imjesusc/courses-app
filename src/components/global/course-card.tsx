e249e57c9836da7b5247fbee932feac2076b5cbe
imjesusc <iamjesus.ds@gmail.com>
2024-05-29 20:25:48 -0400

    refactor: Update CourseCard  jsx semantic

--- src/components/global/course-card.tsx
+++ src/components/global/course-card.tsx
@@ -4,48 +4,48 @@ import { CourseCardModel } from '@/models'
 import { cn } from '@/utilities'
 import Image from 'next/image'
 import Link from 'next/link'
+import { format } from 'timeago.js'
 import fallback from '../../assets/images/image.png'
-import { Icons } from './icons'
 
-export const CourseCard = ({ id, title, views, createdAt, author, images }: CourseCardModel) => {
+export const CourseCard = ({ id, title, views, publishedAt, author, images }: CourseCardModel) => {
   return (
-    <article
-      className={cn('group relative overflow-hidden rounded-2xl duration-100 hover:bg-secondary hover:opacity-100')}
-    >
-      <button
+    <article className={cn('group relative overflow-hidden rounded-xl p-2 duration-100 hover:bg-secondary/20')}>
+      {/* <button
         className={cn(
           'absolute right-3 top-3 z-50 flex h-[38px] w-[38px] cursor-pointer items-center justify-center rounded-full',
           'opacity-0 transition-opacity duration-200 hover:bg-accent group-hover:opacity-100'
         )}
       >
         <Icons.star size={18} />
-      </button>
+      </button> */}
 
-      <Link href="" className="grid gap-4 rounded-lg p-2">
-        <div className="flex flex-col gap-1">
-          <header>
-            <figure className="h-full w-full overflow-hidden rounded-lg">
-              <Image
-                src={images.default || fallback}
-                alt="Cursos en línea"
-                className="aspect-video h-full w-full rounded-lg bg-background object-cover"
-              />
-            </figure>
-          </header>
+      <div className="flex flex-col gap-2">
+        <Link href="">
+          <figure className="h-full w-full overflow-hidden rounded-lg">
+            <Image
+              src={images.standard || fallback}
+              alt="Cursos en línea"
+              className="aspect-video h-full w-full rounded-lg bg-background object-cover"
+              width={800}
+              height={800}
+            />
+          </figure>
+        </Link>
 
-          <footer className="grid gap-1">
-            <h3 className="text-sm font-semibold">{title}</h3>
-            <small className="text-muted-foreground">{author}</small>
-            <div className="flex items-center gap-1 text-sm text-muted-foreground">
-              <small>
-                {views} {views === 1 ? 'view' : 'views'}
-              </small>
-              <span>•</span>
-              <small>{createdAt}</small>
-            </div>
-          </footer>
-        </div>
-      </Link>
+        <footer className="grid gap-0.5">
+          <h3 className="text-sm font-semibold">{title}</h3>
+          <a target="_blank" rel="noreferrer" href={`https://www.youtube.com/@${author}`}>
+            <small className="line-clamp-2 text-xs text-muted-foreground">@{author}</small>
+          </a>
+          <div className="flex items-center gap-1 text-sm text-muted-foreground">
+            <small>
+              {views} {views === 1 ? 'view' : 'views'}
+            </small>
+            <span>•</span>
+            <small>{format(publishedAt)}</small>
+          </div>
+        </footer>
+      </div>
     </article>
   )
 }
