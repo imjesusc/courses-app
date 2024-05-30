d6c7a559bae6459926c7ab5c6f73f16638e098ae
imjesusc <iamjesus.ds@gmail.com>
2024-05-29 20:23:31 -0400

    feat: Add schema to import enviroments

--- src/enviroments/schema.ts
+++ src/enviroments/schema.ts
@@ -1,8 +1,9 @@
-export const SCHEMA: Record<string, string> = {
+export const SCHEMA = {
   // Turso Database
-  DB_TOKEN: process.env.TURSO_AUTH_TOKEN || '',
   DB_URL: process.env.TURSO_DATABASE_URL || '',
+  DB_TOKEN: process.env.TURSO_AUTH_TOKEN || '',
 
   // Youtube V3
-  YT_V3_API_KEY: process.env.NEXT_YOUTUBE_V3_API_KEY || ''
+  YT_V3_API_KEY: process.env.YOUTUBE_V3_API_KEY || '',
+  YT_URL: process.env.YOUTUBE_API_URL || ''
 }
