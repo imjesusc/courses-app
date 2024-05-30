dace67203c34d756df0038592db7cdc3dfd45d3a
imjesusc <iamjesus.ds@gmail.com>
2024-05-29 20:21:16 -0400

    feat: Add ytPlaylistItems model

---
+++ src/models/yt-playlist-items.model.ts
@@ -0,0 +1,41 @@
+import { PageInfo, Thumbnails } from './yt-items'
+
+export interface ytPlaylistItemsModel {
+  kind: string
+  etag: string
+  nextPageToken: string
+  items: ytPlaylistItemsItemModel[]
+  pageInfo: PageInfo
+}
+
+export interface ytPlaylistItemsItemModel {
+  kind: string
+  etag: string
+  id: string
+  snippet: ytPlaylistItemSnippetModel
+  contentDetails: ytPlaylistItemDetailsModel
+}
+
+export interface ytPlaylistItemDetailsModel {
+  videoId: string
+  videoPublishedAt: Date
+}
+
+export interface ytPlaylistItemSnippetModel {
+  publishedAt: Date
+  channelId: string
+  title: string
+  description: string
+  thumbnails: Thumbnails
+  channelTitle: string
+  playlistId: string
+  position: number
+  resourceId: ytPlaylistItemResourceID
+  videoOwnerChannelTitle: string
+  videoOwnerChannelId: string
+}
+
+export interface ytPlaylistItemResourceID {
+  kind: string
+  videoId: string
+}
