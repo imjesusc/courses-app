fdb654c8f3a0e458b70a86e2733f7a171ee8b513
imjesusc <iamjesus.ds@gmail.com>
2024-05-30 23:19:42 -0400

    feat: Add MiListaPage component

---
+++ src/app/(user)/mi-lista/page.tsx
@@ -0,0 +1,9 @@
+import { MyCoursesSet } from '@/modules/mi-lista/components'
+
+export default function MiListaPage() {
+  return (
+    <div className="container pb-10 pt-4">
+      <MyCoursesSet />
+    </div>
+  )
+}
