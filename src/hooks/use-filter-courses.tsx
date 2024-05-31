33739cd957b55a018c822edff232ae6c003f13ec
imjesusc <iamjesus.ds@gmail.com>
2024-05-31 00:54:24 -0400

    refactor: Update useFilterCourses, added page params

--- src/hooks/use-filter-courses.tsx
+++ src/hooks/use-filter-courses.tsx
@@ -19,8 +19,11 @@ export function useFilterCourses() {
   const handleUpdateCategory = useDebouncedCallback((category: string) => {
     const params = new URLSearchParams(searchParams)
     const prevTerm = params.get('q')
+    const currentPage = params.get('page')
+
     params.delete('q')
     params.delete('categories')
+    params.delete('page')
 
     prevTerm && params.set('q', prevTerm)
 
@@ -29,6 +32,9 @@ export function useFilterCourses() {
       const newCategories = [...categories, category]
       params.set('categories', newCategories.join(','))
       setCategories(newCategories)
+
+      // If page already exists, set the same page
+      currentPage && params.set('page', currentPage)
       return replace(`${pathname}?${params.toString()}`)
     }
 
@@ -37,19 +43,27 @@ export function useFilterCourses() {
     hasItems(updatedCategories) && params.set('categories', updatedCategories.join(','))
     setCategories(updatedCategories || [])
 
+    // If page already exists, set the same page
+    currentPage && params.set('page', currentPage)
+
     return replace(`${pathname}?${params.toString()}`)
   }, 250)
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams)
     const prevCategories = params.get('categories')
+    const currentPage = params.get('page')
 
     params.delete('q')
     params.delete('categories')
+    params.delete('page')
 
     // Check if category already exists and if category already exists set the previous categories
     if (term) params.set('q', term)
     prevCategories && params.set('categories', prevCategories)
+
+    // If page already exists, set the same page
+    currentPage && params.set('page', currentPage)
 
     return replace(`${pathname}?${params.toString()}`)
   }, 250)
