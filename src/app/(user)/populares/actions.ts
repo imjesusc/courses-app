f82548d010a6c1f514ebd07c097425addfea668b
imjesusc <iamjesus.ds@gmail.com>
2024-05-30 21:23:10 -0400

    feat: Add getPopularCourses action and implement in new populares page component

---
+++ src/app/(user)/populares/actions.ts
@@ -0,0 +1,33 @@
+import { courseInfoAdapter } from '@/adapters'
+import { db } from '@/config'
+import { courseInfo } from '@/models'
+import { getPlaylistsInfo } from '@/services'
+
+export const getPopularCourses = async () => {
+  try {
+    const courses = await db.courses.findMany({
+      orderBy: {
+        views: 'desc'
+      },
+      take: 10
+    })
+
+    const courseIds = courses.map((course) => course.courseId)
+    const items = await getPlaylistsInfo(courseIds)
+
+    const updatedCourses = courses.map((course) => {
+      const itemCourse = items?.find((item) => item.id === course.courseId) as courseInfo
+
+      if (itemCourse) {
+        itemCourse.snippet.title = course?.title
+        itemCourse.views = course?.views
+      }
+
+      return courseInfoAdapter(itemCourse)
+    })
+
+    return updatedCourses
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ src/app/(user)/populares/page.tsx
@@ -0,0 +1,25 @@
+import { CourseCard } from '@/components/global'
+import { getPopularCourses } from './actions'
+
+export default async function PopularesPage() {
+  const popularCourses = await getPopularCourses()
+
+  return (
+    <div className="container pb-10 pt-4">
+      <main className="grid gap-y-6 tablet:grid-cols-3 laptop:grid-cols-4">
+        {popularCourses?.map((course, index) => (
+          <CourseCard
+            channelId={course.channelId}
+            id={course.id}
+            key={index}
+            title={course.title}
+            author={course.author}
+            images={course.images}
+            views={course.views}
+            publishedAt={course.publishedAt}
+          />
+        ))}
+      </main>
+    </div>
+  )
+}
