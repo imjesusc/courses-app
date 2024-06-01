54b47658495c5aca38afd3f2aed3c173228c5a0e
imjesusc <iamjesus.ds@gmail.com>
2024-05-31 22:37:37 -0400

    refactor: Rename useMyListCourses hook states

--- src/hooks/use-my-list-courses.tsx
+++ src/hooks/use-my-list-courses.tsx
@@ -2,26 +2,26 @@ import { CourseCardModel } from '@/models'
 import { useMyListStore } from '@/store'
 
 export function useMyListCourses() {
-  const myCourses = useMyListStore((state) => state.myCourses)
-  const setMyCourses = useMyListStore((state) => state.setMyCourses)
+  const { myCourses, savedCourses } = useMyListStore((state) => state)
+  const { setMyCourses, setSavedCourses } = useMyListStore((state) => state)
 
   const handleUpdateMyListCourse = (data: CourseCardModel) => {
-    const index = myCourses.findIndex((course) => course.id === data.id)
+    const index = savedCourses.findIndex((course) => course.playlistId === data.playlistId)
 
     //  When the course exists, remove it
     if (index > -1) {
-      const updatedCourses = [...myCourses]
+      const updatedCourses = [...savedCourses]
       updatedCourses.splice(index, 1)
-      setMyCourses(updatedCourses)
+      setSavedCourses(updatedCourses)
       return
     }
 
     //  When the course doesn't exist, add it
-    const newCourses = [...myCourses, data]
-    setMyCourses(newCourses)
+    const newCourses = [...savedCourses, data]
+    setSavedCourses(newCourses)
   }
 
-  const checkCourseInList = (id: string) => myCourses.some((course) => course.id === id)
+  const checkCourseIsSaved = (playlistId: string) => savedCourses.some((course) => course.playlistId === playlistId)
 
-  return { myCourses, handleUpdateMyListCourse, checkCourseInList }
+  return { myCourses, savedCourses, handleUpdateMyListCourse, checkCourseIsSaved }
 }
