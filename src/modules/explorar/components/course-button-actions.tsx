2ebe1deefe17da352f2fc78afc0f8e89313823ba
imjesusc <iamjesus.ds@gmail.com>
2024-06-01 17:39:36 -0400

    feat: Add  in useMyListCourses hook handlers in course-buttons-action to init new course or restore prev

--- src/modules/explorar/components/course-button-actions.tsx
+++ src/modules/explorar/components/course-button-actions.tsx
@@ -3,20 +3,46 @@
 import { Button } from '@/components/ui'
 import { useMyListCourses } from '@/hooks'
 import { CourseCardModel } from '@/models'
+import { useRouter } from 'next/navigation'
 
-export const CourseButtonActions = ({ courseItem }: { courseItem: CourseCardModel }) => {
-  const { handleUpdateMyListCourse, checkCourseIsSaved } = useMyListCourses()
+export const CourseButtonActions = ({
+  totalCourseItems,
+  courseItem
+}: {
+  totalCourseItems: number
+  courseItem: CourseCardModel
+}) => {
+  const router = useRouter()
+  const { handleUpdateSavedCourses, checkCourseIsSaved, checkCourseIsStarted, handleUpdateMyCourses } =
+    useMyListCourses()
 
   return (
     <footer className="flex gap-4">
-      <Button variant="outline">Iniciar Curso</Button>
+      {checkCourseIsStarted(courseItem?.playlistId) ? (
+        <Button variant="outline" onClick={() => router.push(`/mis-cursos/${courseItem.playlistId}`)}>
+          Reanudar
+        </Button>
+      ) : (
+        <Button
+          variant="outline"
+          onClick={() => {
+            handleUpdateMyCourses({
+              ...courseItem,
+              totalCourseItems
+            })
+            router.push(`/mis-cursos/${courseItem.playlistId}`)
+          }}
+        >
+          Empezar
+        </Button>
+      )}
 
       <Button
         variant={checkCourseIsSaved(courseItem?.playlistId) ? 'secondary' : 'outline'}
-        onClick={() => handleUpdateMyListCourse(courseItem)}
+        onClick={() => handleUpdateSavedCourses(courseItem)}
         className="rounded-full"
       >
-        {checkCourseIsSaved(courseItem?.playlistId) ? 'Quitar de mi lista' : 'Agregar a mi lista'}
+        {checkCourseIsSaved(courseItem?.playlistId) ? 'Quitar de mi lista' : 'Guardar en mi lista'}
       </Button>
     </footer>
   )
