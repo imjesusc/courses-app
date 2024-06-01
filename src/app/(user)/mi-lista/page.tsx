440e8f1f772cdfe58d98ba766e457bdddc0385c6
imjesusc <iamjesus.ds@gmail.com>
2024-05-31 22:53:52 -0400

    feat: Add SavedCoursesSet component in mi-lista page component

--- src/app/(user)/mi-lista/page.tsx
+++ src/app/(user)/mi-lista/page.tsx
@@ -1,9 +1,12 @@
-import { MyCoursesSet } from '@/modules/mi-lista/components'
+import { MyCoursesSet, SavedCoursesSet } from '@/modules/mi-lista/components'
 
 export default function MiListaPage() {
   return (
     <div className="container pb-10 pt-4">
-      <MyCoursesSet />
+      <main className="grid gap-y-6">
+        <MyCoursesSet />
+        <SavedCoursesSet />
+      </main>
     </div>
   )
 }
