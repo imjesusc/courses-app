ed1459b0893f39e3edf462cd1db85e41916ccf29
imjesusc <iamjesus.ds@gmail.com>
2024-06-02 23:05:43 -0400

    refactor: Update nav component

--- src/components/global/nav.tsx
+++ src/components/global/nav.tsx
@@ -2,69 +2,72 @@
 
 import { Sheet, SheetContent, SheetHeader } from '@/components/ui'
 import { navLinks } from '@/config'
-import { useMediaQuery } from '@/hooks'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import { Button } from '../ui'
 import { Icons } from './icons'
 
 export const Nav = ({ children }: { children?: React.ReactNode }) => {
   const pathname = usePathname()
-  const isMobile = useMediaQuery('(max-width: 768px)')
+  const router = useRouter()
   const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <>
-      {isMobile ? (
-        <div className="h-16">
-          <header className="container sticky left-0 top-0 flex h-full w-full items-center justify-between">
-            <Button variant={'ghost'} size={'icon'} onClick={() => setMenuOpen(true)}>
-              <Icons.alignLeft size={18} />
-            </Button>
+      <div className="hidden h-16 tablet:block">
+        <header className="container sticky left-0 top-0 flex h-full w-full items-center justify-between">
+          <nav>
+            <ul className="flex items-center gap-6">
+              {navLinks.map(({ label, href }) => (
+                <li key={label}>
+                  <Link className={pathname === href ? 'text-muted-foreground' : ''} href={href}>
+                    {label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-            {menuOpen && (
-              <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
-                <SheetContent side={'left'} className="w-full">
-                  <SheetHeader>
-                    <nav>
-                      <ul className="flex flex-col items-start gap-6 p-4">
-                        {navLinks.map(({ label, href }) => (
-                          <li key={label}>
-                            <Link className={pathname === href ? 'text-muted-foreground underline' : ''} href={href}>
-                              {label}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </nav>
-                  </SheetHeader>
-                </SheetContent>
-              </Sheet>
-            )}
+          {children}
+        </header>
+      </div>
+      {/* Mobile */}
+      <div className="h-16 tablet:hidden">
+        <header className="container sticky left-0 top-0 flex h-full w-full items-center justify-between">
+          <Button variant={'ghost'} size={'icon'} onClick={() => setMenuOpen(true)}>
+            <Icons.alignLeft size={18} />
+          </Button>
 
-            {children}
-          </header>
-        </div>
-      ) : (
-        <div className="h-16">
-          <header className="container sticky left-0 top-0 flex h-full w-full items-center justify-between">
-            <nav>
-              <ul className="flex items-center gap-6">
-                {navLinks.map(({ label, href }) => (
-                  <li key={label}>
-                    <Link className={pathname === href ? 'text-muted-foreground' : ''} href={href}>
-                      {label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
+          {menuOpen && (
+            <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
+              <SheetContent side={'left'} className="w-full">
+                <SheetHeader>
+                  <nav>
+                    <ul className="flex flex-col items-start gap-6 p-4">
+                      {navLinks.map(({ label, href }) => (
+                        <li key={label}>
+                          <button
+                            onClick={() => {
+                              router.push(href)
+                              setMenuOpen(false)
+                            }}
+                            className={pathname === href ? 'text-muted-foreground underline' : ''}
+                          >
+                            {label}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </nav>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          )}
 
-            {children}
-          </header>
-        </div>
-      )}
+          {children}
+        </header>
+      </div>
     </>
   )
 }
